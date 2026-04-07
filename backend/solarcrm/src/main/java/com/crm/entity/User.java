cc4150a3698035419265f1d309426902f58e118d
archanapri <archana1996.vtalent@gmail.com>
2026-04-07 16:20:49 +0530

    updated in user java class

--- backend/solarcrm/src/main/java/com/crm/entity/User.java
+++ backend/solarcrm/src/main/java/com/crm/entity/User.java
@@ -10,9 +10,11 @@ import jakarta.persistence.Enumerated;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.Table;
 import jakarta.validation.constraints.Pattern;
 
 @Entity
+@Table(name = "users")
 public class User {
 	
     @Id
