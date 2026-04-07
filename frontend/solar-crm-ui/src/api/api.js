22ddefd98ee7d01c243701f8588ec281916965d5
archanapri <archana1996.vtalent@gmail.com>
2026-04-07 16:56:28 +0530

    updated React api.js file with render url

--- frontend/solar-crm-ui/src/api/api.js
+++ frontend/solar-crm-ui/src/api/api.js
@@ -1,7 +1,9 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "http://localhost:8080/api"
+ // baseURL: "http://localhost:8080/api"
+ baseURL: "https://solarcrm-backend-y32q.onrender.com/api"
+
 });
 
 api.interceptors.request.use(config => {
